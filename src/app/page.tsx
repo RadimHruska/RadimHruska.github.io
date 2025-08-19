63d7970482b2cbb6d1630c9ddbd71fdeb2e58b4f
RadimHruska <123845703+RadimHruska@users.noreply.github.com>
2025-08-19 20:39:03 +0200

    removed projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import Footer from '../components/layout/Footer';
 import Hero from '../components/sections/Hero';
 import About from '../components/sections/About';
 import Experience from '../components/sections/Experience';
-import Projects from '../components/sections/Projects';
 import Contact from '../components/sections/Contact';
 
 export default function Home() {
@@ -20,7 +19,6 @@ export default function Home() {
         <Hero />
         <About />
         <Experience />
-        <Projects />
         <Contact />
         <Footer />
       </div>
